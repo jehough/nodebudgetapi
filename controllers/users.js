0063cfa44fadd5c5381c4d71665a04eddad6e198
jehough <teachlife@outlook.com>
2020-04-28 08:30:29 -0400

    adds create function

--- controllers/users.js
+++ controllers/users.js
@@ -1,2 +1,24 @@
 const bcrypt = require('bcrypt');
-const jwt = require("jsonwebtoken")
+const jwt = require("jsonwebtoken");
+const User = require('../models/user');
+
+module.exports = {
+    create
+};
+
+async function create(userParam) {
+    if( await User.findOne({emai: userParam.email})){
+        throw "Email is already registered.";
+    }
+
+    const user = new User(userParam);
+
+    if (userParam.password){
+        user.hash = bcrypt.hashSync(userParam.password, 12);
+    }
+    else {
+        throw "Must include Password!"
+    }
+
+    await user.save();
+}
