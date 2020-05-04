eab37c04cb72423fdcc7492afdc2c22e2341f97c
jehough <teachlife@outlook.com>
2020-05-04 19:55:37 -0400

    adds user and category relationships to budget

--- models/budget.js
+++ models/budget.js
@@ -3,7 +3,15 @@ const Schema = mongoose.Schema;
 
 const budgetSchema = new Schema ({
     Name: {type: "string", required: true},
-    available: {type: 'Number', default: 0}
+    available: {type: 'Number', default: 0},
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: "User"
+    },
+    categories: [{
+        type: Schema.Types.ObjectId,
+        ref: "Category"
+    }]
 })
 
 module.exports = mongoose.model("Transaction", transactionSchema);
