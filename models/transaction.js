fb3e93b52b83fd67f743147301ec0903ce514027
jehough <teachlife@outlook.com>
2020-05-04 19:59:07 -0400

    adds relationship to transaction

--- models/transaction.js
+++ models/transaction.js
@@ -4,7 +4,11 @@ const Schema = mongoose.Schema;
 const transactionSchema = new Schema ({
     description: {type: "string", required: true},
     amount: {type: 'Number', required: true},
-    date: {type: 'Date', default: Date.now}
+    date: {type: 'Date', default: Date.now},
+    category: {
+        type: Schema.Types.ObjectId,
+        ref: "Category"
+    }
 })
 
 module.exports = mongoose.model("Transaction", transactionSchema);
